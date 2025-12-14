810beea586e40ded571a9b7fdaf4b7441ad680c5
Kagami <shimura.kyohei7617@gmail.com>
2025-12-14 21:08:58 +0900

    feat: loading.tsxでLoad処理

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+	return (
+		<main className="p-8">
+			<h1 className="text-4xl font-bold mb-4">Users</h1>
+			<p className="animate-pulse text-gray-500">Now loading...</p>
+		</main>
+	);
+}
