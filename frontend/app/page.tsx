6ef715da8304ccb94f6d90011c118f637f4336c0
Kagami <shimura.kyohei7617@gmail.com>
2025-12-14 21:10:18 +0900

    refactor: server component
    use clientから非同期にしてserver componentに変更
    Dokerにアクセス変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,43 +1,31 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 type User = {
 	id: number;
 	name: string;
 };
 
-export default function Home() {
-	const [users, setUsers] = useState<User[]>([]);
-	const [error, setError] = useState<string | null>(null);
+// コンポーネント自体を async にする
+export default async function Home() {
+	// Docker内部通信に backend:8000
+	const res = await fetch("http://backend:8000/users", { cache: "no-store" });
 
-	useEffect(() => {
-		fetch("http://localhost:8000/users")
-			.then((res) => {
-				if (!res.ok) throw new Error("Network response was not ok");
-				return res.json();
-			})
-			.then((data) => setUsers(data))
-			.catch((err) => {
-				console.error(err);
-				setError("Failed to get data");
-			});
-	}, []);
+	if (!res.ok) {
+		return (
+			<p className="text-4xl font-bold text-red-500 mb-4 p-8">
+				Error loading users
+			</p>
+		);
+	}
+
+	const users: User[] = await res.json();
+
 	return (
 		<main className="p-8">
-			<h1 className="text-4xl font-red-500 mb-4">Users</h1>
-
-			{error && <p className="text-red-500 mb-4">{error}</p>}
-
-			{users.length === 0 && !error ? (
-				<p>Loading...</p>
-			) : (
-				<ul className="list-disc pl-5">
-					{users.map((user) => (
-						<li key={user.id}>{user.name}</li>
-					))}
-				</ul>
-			)}
+			<h1 className="text-4xl font-bold mb-4">Users</h1>
+			<ul className="list-disc pl-5">
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
 		</main>
 	);
 }
