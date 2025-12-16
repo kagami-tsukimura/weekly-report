25a16c9242e0f5ec271856d515a10c1a6e1c6ed9
Kagami <shimura.kyohei7617@gmail.com>
2025-12-16 22:07:12 +0900

    feat: create get report

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import os
+from typing import List
 
 import psycopg
+import schemas
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -51,4 +53,71 @@ def get_users():
                 users = cur.fetchall()
                 return users
     except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/reports", response_model=List[schemas.Report])
+def get_reports():
+    """Get all weekly reports.
+
+    Args:
+        report (schemas.ReportGet): Report data to get.
+    Returns:
+        List[schemas.Report]: All reports.
+    Raises:
+        HTTPException: Database error.
+    """
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT id, user_id, week_start, done, todo, issues, learning_hours, created_at, updated_at 
+                    FROM reports
+                    ORDER BY week_start DESC
+                    """
+                )
+                reports = cur.fetchall()
+
+                return reports
+    except Exception as e:
+        print(f"Error getting report: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/reports", response_model=schemas.Report)
+def create_report(report: schemas.ReportCreate):
+    """Create a weekly report.
+
+    Args:
+        report (schemas.ReportCreate): Report data to create.
+    Returns:
+        schemas.Report: The created report.
+    Raises:
+        HTTPException: Database error.
+    """
+
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO reports (user_id, week_start, done, todo, issues, learning_hours)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    RETURNING id, user_id, week_start, done, todo, issues, learning_hours, created_at, updated_at
+                    """,
+                    (
+                        report.user_id,
+                        report.week_start,
+                        report.done,
+                        report.todo,
+                        report.issues,
+                        report.learning_hours,
+                    ),
+                )
+                new_report = cur.fetchone()
+
+                return new_report
+    except Exception as e:
+        print(f"Error creating report: {e}")
         raise HTTPException(status_code=500, detail=str(e))
