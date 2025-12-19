8832050baaca008b89205b3a37f5f81807b6e0eb
Kagami <shimura.kyohei7617@gmail.com>
2025-12-19 21:17:48 +0900

    refactor: アーキテクチャ分離

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,31 +1,69 @@
-type User = {
-	id: number;
-	name: string;
-};
+import { getReports } from "@/lib/api";
+import type { Report } from "@/types";
 
 // コンポーネント自体を async にする
 export default async function Home() {
-	// Docker内部通信に backend:8000
-	const res = await fetch("http://backend:8000/users", { cache: "no-store" });
+	let reports: Report[] = [];
+	let error: string = "";
+	const unknownErrorMsg: string = "Unknown error occured";
 
-	if (!res.ok) {
-		return (
-			<p className="text-4xl font-bold text-red-500 mb-4 p-8">
-				Error loading users
-			</p>
-		);
+	try {
+		reports = await getReports();
+	} catch (e) {
+		if (e instanceof Error) {
+			error = e.message;
+		} else {
+			error = unknownErrorMsg;
+		}
 	}
 
-	const users: User[] = await res.json();
+	if (error) {
+		return <p className="text-4xl font-bold text-red-500 mb-4 p-8">{error}</p>;
+	}
 
 	return (
 		<main className="p-8">
-			<h1 className="text-4xl font-bold mb-4">Users</h1>
-			<ul className="list-disc pl-5">
-				{users.map((user) => (
-					<li key={user.id}>{user.name}</li>
+			<h1 className="text-4xl font-bold mt-20 mb-4">Reports</h1>
+			<div className="flex flex-col gap-6">
+				{reports.map((report) => (
+					<div
+						key={report.id}
+						className="border border-gray-700 rounded-xl p-6"
+					>
+						<div className="text-xl font-bold text-gray-100 mb-4">
+							Week Of {report.week_start}
+						</div>
+						<div className="space-y-3">
+							<div>
+								<span className="font-bold text-green-400 mr-2">
+									Done✅
+									<span className="text-gray-300 ml-2">{report.done}</span>
+								</span>
+							</div>
+							<div>
+								<span className="font-bold text-amber-400 mr-2">
+									ToDo🚧
+									<span className="text-gray-300 ml-2">{report.todo}</span>
+								</span>
+							</div>
+							<div>
+								<span className="font-bold text-blue-400 mr-2">
+									Learning📘
+									<span className="text-gray-300 ml-2">
+										{report.learning_hours}h
+									</span>
+								</span>
+							</div>
+							<div>
+								<span className="font-bold text-red-400 mr-2">
+									Issue🚨
+									<span className="text-gray-300 ml-2">{report.issues}</span>
+								</span>
+							</div>
+						</div>
+					</div>
 				))}
-			</ul>
+			</div>
 		</main>
 	);
 }
