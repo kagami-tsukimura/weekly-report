d9a550dd57bda4057eda5293a55f9787b91c4ed9
Kagami <shimura.kyohei7617@gmail.com>
2025-12-19 21:17:28 +0900

    refactor: loading reports

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
 	return (
 		<main className="p-8">
-			<h1 className="text-4xl font-bold mb-4">Users</h1>
+			<h1 className="text-4xl font-bold mt-20 mb-4">Reports</h1>
 			<p className="animate-pulse text-gray-500">Now loading...</p>
 		</main>
 	);
